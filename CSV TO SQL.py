5461cdcdac870e6840ea454db5af4c7fd49cd8e7
Sabik <92353902+sabik360@users.noreply.github.com>
2023-09-24 23:42:28 +0530

    Adding Python Code for Conversion

---
+++ CSV TO SQL.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import glob, os
+from sqlalchemy import create_engine
+
+for file in glob.glob("*.csv"):
+    df = pd.read_csv(file)
+    
+    # Create SQLAlchemy engine to connect to MySQL Database
+    engine = create_engine("mysql+mysqldb://root:<PASSWORD>@localhost:3306/airbnb")
+    
+    # Convert dataframe to sql table                                   
+    df.to_sql(file[:-4], engine, index=False)
+    
+    print('done')
